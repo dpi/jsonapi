63932c6d8f0cf02f6adbd45962a00ad86be554e0
wimleers <wimleers@99777.no-reply.drupal.org>
2018-09-06 09:47:52 -0600

    Issue #2987606 by Wim Leers, gabesullice, e0ipso, omkar06: Remove config mutation tests from EntityResourceTest

--- tests/src/Kernel/Controller/EntityResourceTest.php
+++ tests/src/Kernel/Controller/EntityResourceTest.php
@@ -3,7 +3,6 @@
 namespace Drupal\Tests\jsonapi\Kernel\Controller;
 
 use Drupal\Component\Serialization\Json;
-use Drupal\Core\Config\ConfigException;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Field\EntityReferenceFieldItemListInterface;
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
@@ -475,27 +474,6 @@ class EntityResourceTest extends JsonapiKernelTestBase {
   /**
    * @covers ::createIndividual
    */
-  public function testCreateIndividualConfig() {
-    $node_type = NodeType::create([
-      'type' => 'test',
-      'name' => 'Test Type',
-      'description' => 'Lorem ipsum',
-    ]);
-    Role::load(Role::ANONYMOUS_ID)
-      ->grantPermission('administer content types')
-      ->save();
-    $resource_type = new ResourceType('node', 'article', NULL);
-    $response = $this->entityResource->createIndividual($resource_type, $node_type, new Request());
-    // As a side effect, the node type will also be saved.
-    $this->assertNotEmpty($node_type->id());
-    $this->assertInstanceOf(JsonApiDocumentTopLevel::class, $response->getResponseData());
-    $this->assertEquals('test', $response->getResponseData()->getData()->id());
-    $this->assertEquals(201, $response->getStatusCode());
-  }
-
-  /**
-   * @covers ::createIndividual
-   */
   public function testCreateIndividualDuplicateError() {
     Role::load(Role::ANONYMOUS_ID)
       ->grantPermission('create article content')
@@ -566,87 +544,6 @@ class EntityResourceTest extends JsonapiKernelTestBase {
   }
 
   /**
-   * @covers ::patchIndividual
-   * @dataProvider patchIndividualConfigProvider
-   */
-  public function testPatchIndividualConfig($values) {
-    // List of fields to be ignored.
-    $ignored_fields = ['uuid', 'entityTypeId', 'type'];
-    $node_type = NodeType::create([
-      'type' => 'test',
-      'name' => 'Test Type',
-      'description' => '',
-    ]);
-    $node_type->save();
-
-    $parsed_node_type = NodeType::create($values);
-    Role::load(Role::ANONYMOUS_ID)
-      ->grantPermission('administer content types')
-      ->save();
-    Role::load(Role::ANONYMOUS_ID)
-      ->grantPermission('edit any article content')
-      ->save();
-    $payload = Json::encode([
-      'data' => [
-        'type' => 'node_type',
-        'id' => $node_type->uuid(),
-        'attributes' => $values,
-      ],
-    ]);
-    $request = new Request([], [], [], [], [], [], $payload);
-
-    $resource_type = new ResourceType('node', 'article', NULL);
-    $response = $this->entityResource->patchIndividual($resource_type, $node_type, $parsed_node_type, $request);
-
-    // As a side effect, the node will also be saved.
-    $this->assertInstanceOf(JsonApiDocumentTopLevel::class, $response->getResponseData());
-    $updated_node_type = $response->getResponseData()->getData();
-    $this->assertInstanceOf(NodeType::class, $updated_node_type);
-    // If the field is ignored then we should not see a difference.
-    foreach ($values as $field_name => $value) {
-      in_array($field_name, $ignored_fields) ?
-        $this->assertNotSame($value, $node_type->get($field_name)) :
-        $this->assertSame($value, $node_type->get($field_name));
-    }
-    $this->assertEquals(200, $response->getStatusCode());
-  }
-
-  /**
-   * Provides data for the testPatchIndividualConfig.
-   *
-   * @return array
-   *   The input data for the test function.
-   */
-  public function patchIndividualConfigProvider() {
-    return [
-      [['description' => 'PATCHED', 'status' => FALSE]],
-      [[]],
-    ];
-  }
-
-  /**
-   * @covers ::patchIndividual
-   * @dataProvider patchIndividualConfigFailedProvider
-   */
-  public function testPatchIndividualFailedConfig($values) {
-    $this->setExpectedException(ConfigException::class);
-    $this->testPatchIndividualConfig($values);
-  }
-
-  /**
-   * Provides data for the testPatchIndividualFailedConfig.
-   *
-   * @return array
-   *   The input data for the test function.
-   */
-  public function patchIndividualConfigFailedProvider() {
-    return [
-      [['uuid' => 'PATCHED']],
-      [['type' => 'article', 'status' => FALSE]],
-    ];
-  }
-
-  /**
    * @covers ::deleteIndividual
    */
   public function testDeleteIndividual() {
@@ -666,34 +563,6 @@ class EntityResourceTest extends JsonapiKernelTestBase {
       ->getStorage('node')
       ->getQuery()
       ->condition('nid', $nid)
-      ->count()
-      ->execute();
-    $this->assertEquals(0, $count);
-    $this->assertNull($response->getResponseData());
-    $this->assertEquals(204, $response->getStatusCode());
-  }
-
-  /**
-   * @covers ::deleteIndividual
-   */
-  public function testDeleteIndividualConfig() {
-    $node_type = NodeType::create([
-      'type' => 'test',
-      'name' => 'Test Type',
-      'description' => 'Lorem ipsum',
-    ]);
-    $id = $node_type->id();
-    $node_type->save();
-    Role::load(Role::ANONYMOUS_ID)
-      ->grantPermission('administer content types')
-      ->save();
-    $resource_type = new ResourceType('node', 'article', NULL);
-    $response = $this->entityResource->deleteIndividual($resource_type, $node_type, new Request());
-    // As a side effect, the node will also be deleted.
-    $count = $this->container->get('entity_type.manager')
-      ->getStorage('node_type')
-      ->getQuery()
-      ->condition('type', $id)
       ->count()
       ->execute();
     $this->assertEquals(0, $count);
